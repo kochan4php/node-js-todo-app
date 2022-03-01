92ce72639732602ad7d6f99daae6c6ccf13f6a3e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-03-01 20:20:36 +0700

    initial commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const connectDB = require("./server/database/database");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Set ejs
 app.set("views", __dirname + "/views");
